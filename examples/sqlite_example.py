83a4104cbf9e620a6f95e4209555ec6c3c80d80d
Nick Beeuwsaert <nick.beeuwsaert@gmail.com>
2017-11-28 15:41:04 -0600

    Put too much work into the sqlite example

--- examples/sqlite_example.py
+++ examples/sqlite_example.py
@@ -36,6 +36,7 @@ import sqlite3
 import json
 import time
 import functools
+import io
 
 from mana_cost import ManaCost as ManaCostBase
 import mana_cost
@@ -69,6 +70,129 @@ class ManaCost(ManaCostBase):
     @reify
     def combinations(self):
         return list(super().combinations)
+
+
+def _print_results(cursor):
+    columns = [
+        name
+        for name, *_ in cursor.description
+    ]
+    column_widths = [len(column_name) for column_name in columns]
+    rows = cursor.fetchall()
+    row_widths = [
+        max(len(str(cell)) for cell in column)
+        for column in zip(*rows)
+    ]
+
+    widths = [max(pair) for pair in zip(column_widths, row_widths)]
+
+    row_separator = '+-{}-+'.format(
+        '-+-'.join('-'*width for width in widths)
+    )
+    header_separator = '+={}=+'.format(
+        '=+='.join('='*width for width in widths)
+    )
+
+    def format_row(row):
+        return '| {} |'.format(
+            ' | '.join(
+                str(cell).ljust(width) for cell, width in zip(row, widths)
+            )
+        )
+
+    print(header_separator)
+    print(format_row(columns))
+    print(header_separator)
+
+    for row in rows:
+        print(format_row(row))
+        print(row_separator)
+
+    print('Fetched {} row(s) in {:.4f}s'.format(len(rows), cursor.timing))
+
+
+def timed(fn):
+    @functools.wraps(fn)
+    def wrapped(cursor, *args, **kwargs):
+        elapsed = -time.perf_counter()
+        try:
+            return fn(cursor, *args, **kwargs)
+        finally:
+            elapsed += time.perf_counter()
+            cursor.timing = elapsed
+
+    return wrapped
+
+
+class TimedCursor(sqlite3.Cursor):
+    execute = timed(sqlite3.Cursor.execute)
+    executemany = timed(sqlite3.Cursor.executemany)
+    executescript = timed(sqlite3.Cursor.executescript)
+
+
+class InteractiveConsole:
+    def __init__(self, connection):
+        self._connection = connection
+        self.buffer = io.StringIO()
+
+    def resetbuffer(self):
+        self.buffer.seek(0, io.SEEK_SET)
+        self.buffer.truncate(0)
+
+    def run(self, command):
+        if not sqlite3.complete_statement(command):
+            return True
+
+        cursor = self._connection.cursor(TimedCursor)
+        try:
+            cursor.execute(command)
+
+        # According to PEP-0249 and the python sqlite3 module documentation
+        # Things like syntax errors should be a ProgrammingError
+        # But for whatever reason, sqlite3 raises a OperationalError for
+        # syntax errors
+        except (sqlite3.OperationalError, sqlite3.ProgrammingError) as e:
+            # Just show the error to the user
+            print(e)
+        else:
+            if cursor.description:
+                _print_results(cursor)
+        finally:
+            cursor.close()
+
+        return False
+
+    def interact(self, banner=None):
+        more = False
+
+        if banner is not None:
+            print(banner)
+
+        while True:
+            prompt = "sqlite> " if not more else "   ...> "
+
+            try:
+                line = input(prompt)
+            except EOFError:
+                print('')
+                if more:
+                    more = False
+                    self.resetbuffer()
+                    print("Press again to exit")
+                else:
+                    break
+            else:
+                more = self.push(line)
+
+    def push(self, line):
+        self.buffer.write(line + '\n')
+
+        more = self.run(self.buffer.getvalue())
+
+        if not more:
+            self.resetbuffer()
+
+        return more
 
 
 def import_data(args):
@@ -121,38 +245,19 @@ def query(args):
         lambda arg: ManaCost(arg).max_mana_cost
     )
 
-    elapsed = -time.perf_counter()
-    cursor = connection.execute(args.query)
-    results = cursor.fetchall()
-    elapsed += time.perf_counter()
-    column_names = [col_name for col_name, *_ in cursor.description]
-
-    column_widths = tuple(
-        max(pair)
-        for pair in zip(
-            (max(len(str(value)) for value in col) for col in zip(*results)),
-            (len(col) for col in column_names)
-        )
-    )
-
-    def separator(ch='-'):
-        return '+{}+'.format(
-            '+'.join(ch*(width+2) for width in column_widths)
-        )
-
-    def display_row(row):
-        return '| {} |'.format(' | '.join(
-            str(data).ljust(width)
-            for data, width in zip(row, column_widths)
-        ))
-
-    print(separator())
-    print(display_row(column_names))
-    print(separator('='))
-    for row in results:
-        print(display_row(row))
-        print(separator())
-    print(f"Fetched {len(results)} rows in {elapsed:.4} seconds.")
+    if args.query is None:
+        try:
+            import readline
+        except ImportError:
+            # Readline support couldnt be enabled
+            pass
+        repl = InteractiveConsole(connection)
+        repl.interact('{}\n\nCtrl+D to exit'.format(__doc__))
+    else:
+        cursor = connection.cursor(TimedCursor)
+        cursor.execute(args.query)
+        if cursor.description:
+            _print_results(cursor)
 
 
 if __name__ == '__main__':
@@ -166,7 +271,11 @@ if __name__ == '__main__':
     import_parser.set_defaults(func=import_data)
 
     query_parser = subparsers.add_parser('query')
-    query_parser.add_argument('query')
+    query_parser.add_argument(
+        'query',
+        nargs='?',
+        help="Query to execute, if empty a interactive session will be created"
+    )
     query_parser.set_defaults(func=query)
 
     args = parser.parse_args()
